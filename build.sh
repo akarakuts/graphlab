f175fa7733fd512ff91345dfdc5444e896ccec71
Aleksey Karakuts <38717078+akarakuts@users.noreply.github.com>
2018-12-20 10:28:07 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=graphlab --build-arg "GL_USER_EMAIL=mbox@mbox.com" --build-arg "USER_KEY=1111-2222-3333-4444-5555-6666-7777-8888" .
+docker build -t=graphlab --build-arg "GL_USER_EMAIL=mbox@mbox.com" --build-arg "GL_USER_KEY=1111-2222-3333-4444-5555-6666-7777-8888" .
